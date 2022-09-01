12e7ad9d4e950070b7dc52cc76e84502d94607f3
Omar Vega <83027913+Cieth@users.noreply.github.com>
2022-08-31 19:42:55 -0500

    updated test

--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,28 @@
 const {createRobot} = require('./index.js');
 
-describe('should be inside its limits 0-10', ()=>{
-    it('should increment x value', ()=>{
+describe('Should be inside its limits ', ()=>{
+    it('should not go any further than [0,0] or [10,10]', ()=>{
+        const {moveForward} = createRobot(5,10);
+        expect(moveForward()).toBe("Out of bounds");
+    });
+});
+
+
+describe('Should rotate correctly', ()=>{
+    it('should turn right', ()=>{
+        const {turnRight} = createRobot(5,5);
+        expect(turnRight()).toBe("E");
+    });
+    it('should turn left', ()=>{
         const {turnLeft} = createRobot(5,5);
-        expect(turnLeft()).toBe("E");
-        expect(turnLeft()).toBe("S");
-        expect(turnLeft()).toBe("O");
+        expect(turnLeft()).toBe("W");
     });
 });
 
 describe('Should move forward', ()=>{
     it('should increment y value', ()=>{
         const {moveForward} = createRobot(5,5);
+        expect(moveForward()).toBe(5);
         expect(moveForward()).toBe(6);
     });
 });
