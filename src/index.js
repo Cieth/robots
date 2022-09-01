18e78a9a3c10ff9456f2c942d098bc7d844bc790
Omar Vega <83027913+Cieth@users.noreply.github.com>
2022-08-31 19:47:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,36 +3,37 @@ exports.createRobot = (a, b) => {
     let y = b
     let facing = "N"    //Actual orientation of robot
 
-    function tRigth() {
+    function turnRight() {     // Changes orientation to the rigth based on the actual orientation 
         if (facing == "N") facing = "E"
         else if (facing == "E") facing =  "S"
         else if (facing == "S") facing =  "W"
         else if (facing == "W") facing =  "N"
+        return facing
     }
-    function tLeft() {
+    function turnLeft() {      // Changes orientation to the left based on the actual orientation 
         if (facing == "N") facing =  "W" 
         else if (facing == "W") facing =  "S"
         else if (facing == "S") facing =  "E"
         else if (facing == "E") facing =  "N"
+        return facing
     }
     const moveForward = () =>{
-        if((x>0 && x<10) && (y>0 && y<10)){
+        if((x>0 && x<10) && (y>0 && y<10)){ //If it's inside the grid
             if(facing == "N" ) return y++;
             else if(facing == "E") return x++;
             else if(facing == "S") return y--;
             else if(facing == "W") return x--;
         }else{
-            return "Out of bounds"
+            return "Out of bounds"          //if it's not
         }
     };
-    const turnLeft = () => tLeft(facing);
-    const turnRight = () => tRigth(facing); 
-    const getCoordsAndOrientation = () => `[${x},${y}] Orientation: ${facing}`
+
+    const getCoordsAndOrientation = () => `[${x},${y}] Orientation: ${facing}` 
 
     return {
         moveForward, //- Moves forward
         turnLeft, //- Turns left
         turnRight, //- Turns right
-        getCoordsAndOrientation
+        getCoordsAndOrientation //- Gets coordenates and orientation of the robot
     }
 }
